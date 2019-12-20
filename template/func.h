f74245c2da7c2c78445bbf91ecc5eece0ace97c8
Joshua Davis <hqurve@outlook.com>
2019-12-20 10:41:41 -0400

    Fixed template distribution

--- template/func.h
+++ template/func.h
@@ -4,27 +4,37 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
+/*
+    For illustration a uniform distribution is shown
+*/
 struct params{
-    int i;
+    int n;
 };
 
-#define PARAM_COUNT 0
-#define PARAM_HINTS ""
+#define PARAM_COUNT 1
+#define PARAM_HINTS "[n]"
 
 struct params* getParams(int argc, char** argv){
     struct params* params = malloc(sizeof(struct params));
     if (params == NULL) return params;
-
+    params->n = atoi(argv[0]);
+    if (params->n <1){
+        free(params);
+        return NULL;
+    }
     return params;
 }
 
 void printParams(FILE* file, struct params* params){
-
+    fprintf(file, "n=%d", params->n);
 }
 double generateSample(struct params* params){
-    return ((double)rand() / RAND_MAX);
+    return ((double)rand() / RAND_MAX) * params->n;
 }
 double CDF(struct params* params, double x){
+    x /= params-> n;
+    if (x <0) return 0;
+    if (x >=1) return 1;
     return x;
 };
 
